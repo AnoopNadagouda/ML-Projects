75407bef938ccd4e40ba1fcad4fbfa6120842f08
ANOOP P NADAGOUDA <anoopnadagouda@gmail.com>
2026-05-03 16:51:41 +0530

    EDA and problem statement

--- src/logger.py
+++ src/logger.py
@@ -1,6 +1,7 @@
 import logging
 import os
 from datetime import datetime
+from src.logger import logging
 
 # Define the log file name with a timestamp
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
